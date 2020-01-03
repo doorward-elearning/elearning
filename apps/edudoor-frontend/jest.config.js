29dfc8872c86d87727e6cdba53005f7e6eba435e
Moses Gitau <gitaumoses4@gmail.com>
2020-01-03 19:02:44 +0300

    Add template for edudoor frontend react app

---
+++ apps/edudoor-frontend/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'edudoor-frontend',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/edudoor-frontend'
+};
