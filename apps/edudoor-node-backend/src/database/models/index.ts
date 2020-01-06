ecb480f8a4127d2ebcdbbe8f46f121aedef356ed
Moses Gitau <gitaumoses4@gmail.com>
2020-01-06 21:40:09 +0300

    Refactor the models file

--- apps/edudoor-node-backend/src/database/models/index.ts
+++ apps/edudoor-node-backend/src/database/models/index.ts
@@ -1,55 +1,31 @@
 import { environment } from '../../environments/environment';
-import Answer from './answer';
-import Course from './course';
-import Meeting from './meeting';
-import MeetingRoom from './meetingRoom';
-import MeetingRoomMember from './meetingroommember';
-import Module from './module';
-import ModuleItem from './moduleitem';
-import Organization from './organization';
-import PasswordResets from './passwordresets';
-import Question from './question';
-import Role from './Role';
-import StudentCourse from './StudentCourse';
-import User from './User';
-import UserRole from './UserRole';
-import { Sequelize } from 'sequelize';
-import Tools from '@edudoor/ui/utils/Tools';
+import database from '../../config/database';
 
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const basename = path.basename(__filename);
 const env = environment.environment || 'development';
-const config = require(`${__dirname}/../../config/database.js`)[env];
+const config = database[env];
 
-const modelDefinitions = {
-  Answer,
-  Course,
-  Meeting,
-  MeetingRoom,
-  MeetingRoomMember,
-  Module,
-  ModuleItem,
-  Organization,
-  PasswordResets,
-  Question,
-  Role,
-  StudentCourse,
-  User,
-  UserRole,
-};
+const models = {};
+
 const sequelize = new Sequelize(environment.DATABASE_URL, config);
 
-const models = Object.keys(modelDefinitions).reduce((acc, modelName) => {
-  const model = sequelize.import(modelName, modelDefinitions[modelName]);
-  if (model.associate) {
-    model.associate(models);
-  }
-  model.beforeCreate(model => {
-    model.id = Tools.generateId();
+fs.readdirSync(__dirname)
+  .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
+  .forEach(file => {
+    const model = sequelize.import(path.join(__dirname, file));
+    models[model.name] = model;
   });
 
-  return {
-    ...acc,
-    [modelName]: model,
-  };
-}, {});
+Object.keys(models).forEach(modelName => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models);
+  }
+});
+
+export type Models = typeof models;
 
 export default models;
