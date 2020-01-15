afab23b136609d987d0969226d7fce75df2427cd
Moses Gitau <gitaumoses4@gmail.com>
2020-01-15 13:45:05 +0300

    Fix issue with organization variable

--- apps/edudoor-frontend/src/screens/Students/CreateStudentGroup.tsx
+++ apps/edudoor-frontend/src/screens/Students/CreateStudentGroup.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { PageComponent } from '@edudoor/ui/types';
 import Layout, { LayoutFeatures } from '../Layout';
 import AddGroupForm from '../../components/Forms/AddGroupForm';
+import NotFound from '@edudoor/ui/components/NotFound';
+import Error404 from '../ErrorPages/Error404';
 
 const CreateStudentGroup: React.FunctionComponent<CreateStudentGroupProps> = (props): JSX.Element => {
-  return (
-    <Layout {...props} features={[LayoutFeatures.BREAD_CRUMBS]}>
-      <AddGroupForm />
-    </Layout>
-  );
+  return <Error404 {...props} />;
 };
 
 export interface CreateStudentGroupProps extends PageComponent {}
