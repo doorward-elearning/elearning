845ae44480828cad33bf12afaeab746866882cf1
Moses Gitau <gitaumoses4@gmail.com>
2020-02-17 22:18:11 +0300

    Update search header for conversations

--- libs/chat/src/components/ConversationList/index.tsx
+++ libs/chat/src/components/ConversationList/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './ConversationList.scss';
 import ItemArray from '@edudoor/ui/components/ItemArray';
 import ConversationListItem from '@edudoor/chat/components/ConversationListItem';
+import Search from '@edudoor/ui/components/Search';
 
 const ConversationList: React.FunctionComponent<ConversationListProps> = (props): JSX.Element => {
   return (
     <div className="ed-conversation-list">
+      <Search onChange={() => {}} placeholder="Search conversations..." />
       <ItemArray data={props.conversations}>
         {item => {
           return <ConversationListItem recipient={item.recipient} />;
--- libs/ui/src/components/Search/index.tsx
+++ libs/ui/src/components/Search/index.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { PlainTextField } from '@edudoor/ui/components/Input/TextField';
+import Icon from '@edudoor/ui/components/Icon';
+import './Search.scss';
 
 const Search: React.FunctionComponent<SearchProps> = (props): JSX.Element => {
   const [search, setSearch] = useState('');
@@ -12,20 +14,30 @@ const Search: React.FunctionComponent<SearchProps> = (props): JSX.Element => {
   }, [search]);
   return (
     <PlainTextField
-      placeholder="Search..."
+      placeholder={props.placeholder || 'Search...'}
       labelPosition="none"
+      className="ed-search"
       icon="search"
       onChange={e => {
         setSearch(e.target.value);
       }}
       value={search}
-    />
+    >
+      <Icon
+        icon="close"
+        className="close-icon"
+        onClick={() => {
+          setSearch('');
+        }}
+      />
+    </PlainTextField>
   );
 };
 
 export interface SearchProps {
   search?: string;
   onChange: (search: string) => void;
+  placeholder?: string;
 }
 
 export default Search;
