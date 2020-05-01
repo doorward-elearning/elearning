496ddc556c66ecdd6f0add6257c126bb81904b18
Moses Gitau <gitaumoses4@gmail.com>
2020-05-01 10:18:50 +0300

    Fix issue with meeting room migration

--- apps/edudoor-node-backend/src/database/migrations/20191212281739-update-meeting-rooms.js
+++ apps/edudoor-node-backend/src/database/migrations/20191212281739-update-meeting-rooms.js
@@ -1,4 +1,4 @@
-module.exports =  {
+module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('MeetingRooms');
     await queryInterface.createTable('MeetingRooms', {
@@ -21,41 +21,5 @@ module.exports =  {
     });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('MeetingRooms');
-    return queryInterface.createTable('MeetingRooms', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.TEXT,
-      },
-      sessionId: {
-        type: Sequelize.TEXT,
-      },
-      courseId: {
-        type: Sequelize.TEXT,
-        onDelete: 'cascade',
-        references: {
-          model: 'Courses',
-          key: 'id',
-        },
-      },
-      sessionName: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      status: {
-        type: Sequelize.STRING,
-        defaultValue: 'STARTED',
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
+  down: async (queryInterface, Sequelize) => {},
 };
