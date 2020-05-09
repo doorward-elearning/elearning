1d0a90d87db3c59323252a3e6fb97773a22a3d81
Moses Gitau <gitaumoses4@gmail.com>
2020-05-09 22:28:39 +0300

    Configure entities in typeorm

--- apps/edudoor-backend/src/modules/app/app.module.ts
+++ apps/edudoor-backend/src/modules/app/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as PostgresConnectionStringParser from 'pg-connection-string';
+import entities from '../../entities';
 
+const connectionOptions = PostgresConnectionStringParser.parse(process.env.DATABASE_URL);
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: connectionOptions.host,
+      port: +connectionOptions.port,
+      username: connectionOptions.user,
+      password: connectionOptions.password,
+      database: connectionOptions.database,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([...entities]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
