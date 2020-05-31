ad5df8182c659e3fe7631d21f83be5ab6191f29f
Moses Gitau <gitaumoses4@gmail.com>
2020-05-31 09:02:14 +0300

    Update Assignment submission with preview

---
+++ apps/edudoor-frontend/src/components/Forms/AssignmentSubmissionForm/index.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import BasicForm from '../BasicForm';
+import { submitAssignmentAction } from '../../../reducers/courses/actions';
+import TabLayout from '@edudoor/ui/components/TabLayout';
+import Tab from '@edudoor/ui/components/TabLayout/Tab';
+import TextArea from '@edudoor/ui/components/Input/TextArea';
+import TextField from '@edudoor/ui/components/Input/TextField';
+import FileUploadField from '@edudoor/ui/components/Input/FileUploadField';
+import Api from '../../../services/api';
+import IfElse from '@edudoor/ui/components/IfElse';
+import { Assignment } from '@edudoor/common/models/Assignment';
+import { useHistory } from 'react-router';
+import useForm, { UseForm } from '@edudoor/ui/hooks/useForm';
+import { useSelector } from 'react-redux';
+import { State } from '../../../store';
+
+const AssignmentSubmissionForm: React.FunctionComponent<AssignmentSubmissionFormProps> = ({
+  assignment,
+  initialValues,
+  onSuccess,
+}): JSX.Element => {
+  const state = useSelector((state: State) => state.courses.moduleItemList);
+  const form = useForm();
+  const [currentTab, setCurrentTab] = useState();
+  return (
+    <BasicForm
+      state={state}
+      submitAction={submitAssignmentAction}
+      form={form}
+      showSuccessToast
+      showErrorToast
+      onSuccess={onSuccess}
+      createData={values => [
+        assignment.id,
+        {
+          submission: values.submissionType === 'File Upload' ? values.submission[0].id : values.submission,
+          submissionType: values.submissionType,
+          resubmission: false,
+        },
+      ]}
+      initialValues={initialValues}
+    >
+      {formikProps => {
+        return (
+          <TabLayout
+            onTabChange={tab => {
+              if (currentTab !== tab) {
+                setCurrentTab(tab);
+                formikProps.setValues({
+                  submission: '',
+                  submissionType: ['Text Entry', 'Website URL', 'File Upload'][tab],
+                });
+              }
+            }}
+          >
+            <Tab title="Text Entry">
+              <TextArea name="submission" />
+            </Tab>
+            <Tab title="Website URL">
+              <TextField name="submission" />
+            </Tab>
+            <Tab title="File Upload">
+              <FileUploadField
+                name="submission"
+                uploadHandler={(file, onUploadProgress, cancelHandler) => {
+                  return Api.storage.upload(file, false, onUploadProgress, cancelHandler);
+                }}
+              />
+            </Tab>
+          </TabLayout>
+        );
+      }}
+    </BasicForm>
+  );
+};
+
+export interface AssignmentSubmissionFormProps {
+  assignment: Assignment;
+  initialValues: {
+    submission: any;
+    submissionType: string;
+  };
+  onSuccess: () => void;
+}
+
+export default AssignmentSubmissionForm;
--- apps/edudoor-frontend/src/components/UI/AssignmentView/index.tsx
+++ apps/edudoor-frontend/src/components/UI/AssignmentView/index.tsx
@@ -1,32 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DraftHTMLContent from '@edudoor/ui/components/DraftHTMLContent';
 import { Assignment } from '@edudoor/common/models/Assignment';
-import TabLayout from '@edudoor/ui/components/TabLayout';
-import Tab from '@edudoor/ui/components/TabLayout/Tab';
 import Panel from '@edudoor/ui/components/Panel';
 import Header from '@edudoor/ui/components/Header';
-import BasicForm from '../../Forms/BasicForm';
-import { fetchModuleItems, submitAssignmentAction } from '../../../reducers/courses/actions';
-import useForm from '@edudoor/ui/hooks/useForm';
-import { useSelector } from 'react-redux';
-import { State } from '../../../store';
-import TextArea from '@edudoor/ui/components/Input/TextArea';
-import TextField from '@edudoor/ui/components/Input/TextField';
-import FileUploadField from '@edudoor/ui/components/Input/FileUploadField';
-import Api from '../../../services/api';
 import RoleContainer from '@edudoor/ui/components/RolesManager/RoleContainer';
 import { Roles } from '@edudoor/ui/components/RolesManager';
-import useRoutes from '../../../hooks/useRoutes';
 import { useHistory } from 'react-router';
+import IfElse from '@edudoor/ui/components/IfElse';
+import AssignmentSubmissionForm from '../../Forms/AssignmentSubmissionForm';
 
 const AssignmentView: React.FunctionComponent<AssignmentViewProps> = props => {
-  const form = useForm();
-  const [currentTab, setCurrentTab] = useState();
   const initialValues = {
     submission: '',
     submissionType: '',
   };
-  const state = useSelector((state: State) => state.courses.moduleItemList);
   const history = useHistory();
   return (
     <div className="assignment-view">
@@ -34,56 +21,14 @@ const AssignmentView: React.FunctionComponent<AssignmentViewProps> = props => {
       <RoleContainer roles={[Roles.STUDENT]} showSuperAdmin={false}>
         <Panel style={{ marginTop: 'var(--padding-lg)' }} noBackground>
           <Header size={3}>Submission</Header>
-          <BasicForm
-            state={state}
-            submitAction={submitAssignmentAction}
-            form={form}
-            showSuccessToast
-            showErrorToast
-            onSuccess={() => {
-              history.goBack();
-            }}
-            createData={values => [
-              props.assignment.id,
-              {
-                submission: values.submissionType === 'File Upload' ? values.submission[0].id : values.submission,
-                submissionType: values.submissionType,
-                resubmission: false,
-              },
-            ]}
-            initialValues={initialValues}
-          >
-            {formikProps => {
-              return (
-                <TabLayout
-                  onTabChange={tab => {
-                    if (currentTab !== tab) {
-                      setCurrentTab(tab);
-                      formikProps.setValues({
-                        submission: '',
-                        submissionType: ['Text Entry', 'Website URL', 'File Upload'][tab],
-                      });
-                    }
-                  }}
-                >
-                  <Tab title="Text Entry">
-                    <TextArea name="submission" />
-                  </Tab>
-                  <Tab title="Website URL">
-                    <TextField name="submission" />
-                  </Tab>
-                  <Tab title="File Upload">
-                    <FileUploadField
-                      name="submission"
-                      uploadHandler={(file, onUploadProgress, cancelHandler) => {
-                        return Api.storage.upload(file, false, onUploadProgress, cancelHandler);
-                      }}
-                    />
-                  </Tab>
-                </TabLayout>
-              );
-            }}
-          </BasicForm>
+          <IfElse condition={props.assignment.assignmentSubmission}>
+            <div></div>
+            <AssignmentSubmissionForm
+              assignment={props.assignment}
+              initialValues={initialValues}
+              onSuccess={history.goBack}
+            />
+          </IfElse>
         </Panel>
       </RoleContainer>
     </div>
