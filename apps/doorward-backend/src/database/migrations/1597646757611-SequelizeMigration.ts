88e126da7f1e0f39bc95f0f9f2869e769114dd38
Moses Gitau <gitaumoses4@gmail.com>
2020-08-17 09:50:54 +0300

    Set up typescript compiler to use ttypescript

---
+++ apps/doorward-backend/src/database/migrations/1597646757611-SequelizeMigration.ts
@@ -0,0 +1,9 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class SequelizeMigration1597646757611 implements MigrationInterface {
+  name = 'SequelizeMigration1597646757611';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
