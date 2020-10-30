7044511422cc751dd9550c5011635db5fb568186
Moses Gitau <gitaumoses4@gmail.com>
2020-10-30 13:35:13 +0300

    Fix organization config

--- scripts/buildOrganizationConfig.js
+++ scripts/buildOrganizationConfig.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const colors = require('colors');
+const ncp = require('ncp');
 
 const environments = path.join(__dirname, '../helm/environments');
 
@@ -12,30 +13,48 @@ const mkDir = (pathLike) => {
   }
 };
 
+const copyDirectory = (source, destination) => {
+  return new Promise((resolve) => {
+    ncp(source, destination, {}, resolve);
+  });
+};
+
+const source = path.join(__dirname, '../apps/doorward-backend/src/config/default');
+
 const destinations = [path.join(__dirname, '../dist/apps/doorward-backend/config')];
 
 destinations.forEach((destination) => {
   mkDir(destination);
 });
 
-console.log(colors.cyan('Building organization configuration...'));
-
-directories.forEach((dirName) => {
-  const dir = path.join(environments, dirName);
-  if (fs.lstatSync(dir).isDirectory()) {
-    destinations.forEach((destination) => {
-      if (fs.existsSync(path.join(dir, 'organization.json'))) {
-        console.log(colors.yellow('[Organization]'), colors.yellow(' - ' + dirName));
-        mkDir(path.join(destination, dirName));
-        fs.copyFileSync(path.join(dir, 'organization.json'), path.join(destination, dirName, 'organization.json'));
+const buildOrganizationConfig = async () => {
+  console.log(colors.cyan('Building organization configuration...'));
+  await Promise.all(
+    directories.map(async (dirName) => {
+      const dir = path.join(environments, dirName);
+      if (fs.lstatSync(dir).isDirectory()) {
+        await Promise.all(
+          destinations.map(async (destination) => {
+            copyDirectory(source, path.join(destination, dirName));
+            if (fs.existsSync(path.join(dir, 'organization.json'))) {
+              console.log(colors.yellow('[Organization]'), colors.yellow(' - ' + dirName));
+              mkDir(path.join(destination, dirName));
+              fs.copyFileSync(
+                path.join(dir, 'organization.json'),
+                path.join(destination, dirName, 'organization.json')
+              );
+            }
+            if (fs.existsSync(path.join(dir, 'roles.json'))) {
+              console.log(colors.yellow('[Roles]'), colors.yellow(' - ' + dirName));
+              mkDir(path.join(destination, dirName));
+              fs.copyFileSync(path.join(dir, 'roles.json'), path.join(destination, dirName, 'roles.json'));
+            }
+          })
+        );
       }
-      if (fs.existsSync(path.join(dir, 'roles.json'))) {
-        console.log(colors.yellow('[Roles]'), colors.yellow(' - ' + dirName));
-        mkDir(path.join(destination, dirName));
-        fs.copyFileSync(path.join(dir, 'roles.json'), path.join(destination, dirName, 'roles.json'));
-      }
-    });
-  }
-});
+    })
+  );
+  console.log(colors.green('Config files written to: ' + destinations.join(', ')));
+};
 
-console.log(colors.green('Config files written to: ' + destinations.join(', ')));
+buildOrganizationConfig().then();
