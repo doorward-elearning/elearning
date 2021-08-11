20ac1e8b0530b3b74b73e88a68a2eb96f27d38d9
HKDW509 <hemanth@doorward.tech>
2021-08-11 15:25:42 +0530

    Learners List Page - Display only Username and First Name on Mobile

--- apps/doorward-frontend/src/components/Tables/StudentTable/index.tsx
+++ apps/doorward-frontend/src/components/Tables/StudentTable/index.tsx
@@ -4,34 +4,53 @@ import UserEntity from '@doorward/common/entities/user.entity';
 import translate from '@doorward/common/lang/translate';
 
 const StudentTable: React.FunctionComponent<StudentTableProps> = (props) => {
-  return (
-    <Table
-      {...props}
-      className="student-table"
-      columns={{
-        username: {
-          title: translate('username'),
-        },
-        firstName: {
-          title: translate('firstName'),
-        },
-        lastName: {
-          title: translate('lastName'),
-        },
-        email: {
-          title: translate('email'),
-        },
-        phoneNumber: {
-          title: translate('phoneNumber'),
-        },
-        status: {
-          title: translate('status'),
-        },
-      }}
-      data={props.students}
-      onRowClick={props.onClickStudent}
-    />
+  const isMobile = window.innerWidth <= 500;
+  return(
+   isMobile === true ?  <Table
+    {...props}
+    className="student-table"
+    columns={{
+      username: {
+        title: translate('username'),
+      },
+      firstName: {
+        title: translate('firstName'),
+      },
+      
+    }}
+    data={props.students}
+  
+    onRowClick={props.onClickStudent}
+  /> : <Table
+  {...props}
+  className="student-table"
+  columns={{
+    username: {
+      title: translate('username'),
+    },
+    firstName: {
+      title: translate('firstName'),
+    },
+    lastName: {
+      title: translate('lastName'),
+    },
+    email: {
+      title: translate('email'),
+    },
+    phoneNumber: {
+      title: translate('phoneNumber'),
+    },
+    status: {
+      title: translate('status'),
+    },
+  }}
+  data={props.students}
+
+  onRowClick={props.onClickStudent}
+/>
+   
   );
+  
 };
 
 export interface StudentTableProps extends Omit<TableProps<UserEntity>, 'columns' | 'data'> {
