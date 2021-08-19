f1ce3ea7d780972bc2dfadcd05f5d2076d236473
HKDW509 <hemanth@doorward.tech>
2021-08-19 11:33:01 +0530

    Displaying specified details of Teachers list.

--- apps/doorward-frontend/src/components/Tables/TeachersTable/index.tsx
+++ apps/doorward-frontend/src/components/Tables/TeachersTable/index.tsx
@@ -2,25 +2,38 @@ import React from 'react';
 import Table from '@doorward/ui/components/Table';
 import UserEntity from '@doorward/common/entities/user.entity';
 import translate from '@doorward/common/lang/translate';
+import { DisplayDeviceType } from '@doorward/ui/hooks/useResponsiveness';
 
 const TeachersTable: React.FunctionComponent<TeachersTableProps> = (props) => {
-  return (
-   <Table
-    className="teachers-table"
-    sortable
-    columns={{
-      username: translate('username'),
-      firstName: translate('firstName'),
-      email: translate('email'),
-      status: translate('status'),
-    }}
-    data={props.teachers}
-  />
-  );
+return (
+<Table
+{...props}
+className="teachers-table"
+sortable
+columns={{
+          username: { 
+            title : translate('username'),
+          },
+          firstName: {
+            title : translate('firstName'), 
+          },
+          email: {
+            title : translate('email'), 
+            minDisplay: DisplayDeviceType.DESKTOP
+          },
+          status: { 
+            title :translate('status'),
+            minDisplay: DisplayDeviceType.DESKTOP
+          },
+        }}
+        data={props.teachers}
+/>
+);
 };
 
 export interface TeachersTableProps {
-  teachers: Array<UserEntity>;
+teachers: Array<UserEntity>;
 }
 
 export default TeachersTable;
+
