c4d87b30ca3ffb75292613e63394e718d7de7b19
Moses Gitau <gitaumoses4@gmail.com>
2021-09-22 19:00:57 +0300

    Update organization build config

--- scripts/buildOrganizationConfig.js
+++ scripts/buildOrganizationConfig.js
@@ -1,92 +1,34 @@
-const fs = require('fs');
 const path = require('path');
-const YAML = require('yaml');
-const _ = require('lodash');
 const colors = require('colors');
-const ncp = require('ncp');
-const beautify = require('js-beautify');
+const fs = require('fs');
 
-const source = path.join(__dirname, '../apps/doorward-backend/src/config/default');
 const environments = path.join(__dirname, '../elearning-ops/helm/charts/apps/config');
 
-const organizationFiles = fs.existsSync(environments)
-  ? fs.readdirSync(environments).filter((name) => {
-      return name.endsWith('yaml') && !['Chart.yaml', 'secrets.yaml', 'values.yaml'].includes(name);
-    })
-  : [];
-
-const mkDir = (pathLike) => {
-  if (!fs.existsSync(pathLike)) {
-    fs.mkdirSync(pathLike);
-  }
-};
-
-const copyDirectory = (source, destination) => {
-  return new Promise((resolve) => {
-    ncp(source, destination, {}, resolve);
-  });
-};
-
-const destinations = [path.join(__dirname, '../dist/apps/doorward-backend/config')];
-
-destinations.forEach((destination) => {
-  mkDir(destination);
-});
-
-const mergeConfig = (config) => {
-  const map = [
-    { file: 'meetings.config.json', path: 'meetings.default' },
-    { file: 'meetings.config.moderator.json', path: 'meetings.moderator' },
-    { file: 'meetings.config.publisher.json', path: 'meetings.publisher' },
-    { file: 'meetings.config.subscriber.json', path: 'meetings.subscriber' },
-    { file: 'meetings.interface.json', path: 'meeting-interface.default' },
-    { file: 'meetings.interface.moderator.json', path: 'meeting-interface.moderator' },
-    { file: 'meetings.interface.publisher.json', path: 'meeting-interface.publisher' },
-    { file: 'meetings.interface.subscriber.json', path: 'meeting-interface.subscriber' },
-    { file: 'organization.json', path: 'organization' },
-  ];
-
-  const organizationName = config.customer;
-
-  map.forEach((item) => {
-    const existing = JSON.parse(fs.readFileSync(path.join(source, item.file)).toString());
-
-    let result = existing;
-
-    const destination = _.get(config, item.path);
-
-    if (destination) {
-      result = _.merge({}, existing, destination);
-    }
-
-    destinations.map((destination) => {
-      mkDir(path.join(destination, organizationName));
-      fs.writeFileSync(
-        path.join(destination, organizationName, item.file),
-        beautify.js_beautify(JSON.stringify(result))
-      );
-    });
-  });
-};
-
-const createFile = (organizationName) => {
-  console.log(colors.yellow(' - ' + organizationName));
-
-  if (fs.existsSync(path.join(environments, organizationName))) {
-    const file = fs.readFileSync(path.join(environments, organizationName), 'utf8');
-    const destination = YAML.parse(file);
-
-    mergeConfig(destination);
-  }
-};
+const destinations = [path.join(__dirname, '../dist/apps/doorward-backend/config/organization.yaml')];
 
 const buildOrganizationConfig = async () => {
-  console.log(colors.cyan('Building organization configuration... [' + organizationFiles + ']'));
+  const customer = process.env.DEFAULT_ORGANIZATION_NAME;
 
-  organizationFiles.forEach((organizationName) => {
-    createFile(organizationName);
-  });
-  console.log(colors.green('Config files written to: ' + destinations.join(', ')));
+  if (customer) {
+    console.log(colors.cyan('Building organization configuration... [' + customer + ']'));
+
+    const configFile = path.join(environments, customer + '.yaml');
+
+    if (!fs.existsSync(configFile)) {
+      console.error(colors.red('Config file does not exist: ' + configFile));
+      return;
+    }
+
+    const configFileContent = fs.readFileSync(configFile, 'utf8');
+
+    await Promise.all(
+      destinations.map(async (destination) => {
+        fs.writeFileSync(destination, configFileContent);
+      })
+    );
+  } else {
+    console.error(colors.red('Env variable: DEFAULT_ORGANIZATION_NAME is not defined.'));
+  }
 };
 
 buildOrganizationConfig().then();
