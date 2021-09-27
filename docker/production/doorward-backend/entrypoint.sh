aa6ed0f2cc00d4950508517f11d6e2d63beb9f8b
Moses Gitau <gitaumoses4@gmail.com>
2021-09-27 14:46:12 +0300

    Update backend dockerfile with organization migrations

--- docker/production/doorward-backend/entrypoint.sh
+++ docker/production/doorward-backend/entrypoint.sh
@@ -4,8 +4,12 @@ export NODE_ENV='production';
 
 cd /usr/app/
 
-yarn db:thala:migrate
+# Initialize database for organization information
+yarn db:org:migrate
+yarn db:org:seed
 
+# Initialize database for organizations
+yarn db:thala:migrate
 yarn db:thala:seed
 
 cd /usr/app/dist/apps/doorward-backend && node main.js
