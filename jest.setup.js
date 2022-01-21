f191b49453a9cc4c060b7176628aea196d873b3f
Moses Gitau <gitaumoses4@gmail.com>
2022-01-21 14:27:08 +0300

    Fix tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,14 @@
-const configureLang = require('./libs/common/src/lang/frontend.config');
+import configureLang from './libs/common/src/lang/frontend.config';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 const dotenv = require('dotenv');
 
 // Use the .test.env file for testing
 dotenv.config({ path: './.env' });
 
-configureLang(process.env.REACT_APP_BASE_URL).then();
+configureLang(process.env.REACT_APP_BASE_URL).then().catch();
+
+Enzyme.configure({ adapter: new Adapter() });
 
 jest.setTimeout(30000);
--- libs/ui/src/components/Anchor/__test__/Anchor.test.tsx
+++ libs/ui/src/components/Anchor/__test__/Anchor.test.tsx
@@ -1,25 +1,15 @@
 import * as React from 'react';
-
-import Icon from '@doorward/ui/components/Icon';
-import renderer from 'react-test-renderer';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import Anchor from '../index';
 
+describe('Anchor test suit', () => {
+  test('render', () => {
+    const wrapper = shallow(<Anchor />);
+    expect(wrapper.exists()).toBe(true);
+  });
 
-describe("Anchor test suit", ()=>{
-    
-    test("render",()=>{
-        const wrapper = shallow(<Anchor />)
-        expect(wrapper.exists()).toBe(true)
-        
-    })
-
-    test(" check the link",()=>{
-        const wrapper = shallow(<Anchor href='' />)
-        wrapper.find('input').simulate('change',{taget:{value: 'www.google.com'}})
-        expect(wrapper.find('input').props().value).toEqual('www.google.com')
-    })
-
-    test("")
-
+  test(' check the link', () => {
+    const wrapper = shallow(<Anchor href='www.google.com' />);
+    expect(wrapper.find('a').props().href).toEqual('www.google.com');
+  });
 });
